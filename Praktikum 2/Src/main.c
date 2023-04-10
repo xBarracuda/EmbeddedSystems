fcaf61b3d16a0cda56f6ee7fddff1811e4a9b83a
Aruetiise <julius@becker-julius.de>
2023-04-10 18:51:39 +0200

    Ampelschaltung implementiert

--- Praktikum 2/Src/main.c
+++ Praktikum 2/Src/main.c
@@ -24,33 +24,104 @@
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
-void delay(void){
-	for(volatile uint32_t i = 0; i<500000; i++);
+
+//Zeit in Sekunden
+void delay(int time){
+	for(volatile uint32_t i = 0; i<500000*time; i++);
 }
+
+void lauflichtInit(GPIO_Handle_t * LED);
+void lauflicht(GPIO_Handle_t * LED);
+void ampelInit(GPIO_Handle_t* LED_Rot,GPIO_Handle_t* LED_Gelb,GPIO_Handle_t* LED_Gruen);
 
 int main(void)
 {
-	GPIO_Handle_t LED[4];
+	//Lauflicht
+	/*GPIO_Handle_t LED_Lauflicht[4];
+	lauflichtInit(LED_Lauflicht);
+	for(int i=0; i<2; i++)
+		lauflicht(LED_Lauflicht);*/
 
+
+	GPIO_Handle_t LED_Rot, LED_Gelb, LED_Gruen;
+
+	ampelInit(&LED_Rot, &LED_Gelb, &LED_Gruen);
+
+	for(;;){
+		GPIO_WriteToOutputPin(&LED_Rot, ENABLE);
+		GPIO_WriteToOutputPin(&LED_Gelb, DISABLE);
+		GPIO_WriteToOutputPin(&LED_Gruen, DISABLE);
+
+		delay(5);
+
+		GPIO_WriteToOutputPin(&LED_Rot, ENABLE);
+		GPIO_WriteToOutputPin(&LED_Gelb, ENABLE);
+		GPIO_WriteToOutputPin(&LED_Gruen, DISABLE);
+
+		delay(5);
+
+		GPIO_WriteToOutputPin(&LED_Rot, DISABLE);
+		GPIO_WriteToOutputPin(&LED_Gelb, DISABLE);
+		GPIO_WriteToOutputPin(&LED_Gruen, ENABLE);
+
+		delay(5);
+
+		GPIO_WriteToOutputPin(&LED_Rot, DISABLE);
+		GPIO_WriteToOutputPin(&LED_Gelb, ENABLE);
+		GPIO_WriteToOutputPin(&LED_Gruen, DISABLE);
+
+		delay(5);
+	}
+}
+
+void ampelInit(GPIO_Handle_t* LED_Rot,GPIO_Handle_t* LED_Gelb,GPIO_Handle_t* LED_Gruen){
+
+	//Ampelschaltung
+	GPIO_PinConfig_t baseConfig;
+	baseConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	baseConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+	baseConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	baseConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	LED_Rot->pGPIOx = (GPIO_RegDef_t *)GPIOD_BASEADDR;
+	LED_Rot->GPIO_PinConfig = baseConfig;
+	LED_Rot->GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+
+	LED_Gelb->pGPIOx = (GPIO_RegDef_t *)GPIOD_BASEADDR;
+	LED_Gelb->GPIO_PinConfig = baseConfig;
+	LED_Gelb->GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_1;
+
+	LED_Gruen->pGPIOx = (GPIO_RegDef_t *)GPIOD_BASEADDR;
+	LED_Gruen->GPIO_PinConfig = baseConfig;
+	LED_Gruen->GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_2;
+
+	GPIO_Init(LED_Rot);
+	GPIO_Init(LED_Gelb);
+	GPIO_Init(LED_Gruen);
+}
+
+void lauflichtInit(GPIO_Handle_t * LED){
 	for	(int i = 0; i < 4; i++){
 		LED[i].pGPIOx = (GPIO_RegDef_t *)GPIOD_BASEADDR;
 		LED[i].GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15 - i;
 		LED[i].GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+		LED[i].GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+		LED[i].GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+		LED[i].GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 		GPIO_Init(&LED[i]);
 	}
 
 	for(int i = 0; i < 4; i++){
 		GPIO_WriteToOutputPin(&LED[i], DISABLE);
 	}
+}
+
+void lauflicht(GPIO_Handle_t * LED){
 	// GPIO_WriteToOutputPort((GPIO_RegDef_t *)GPIOD_BASEADDR, 0xFF);
-	for(;;){
-		for (int i = 0; i < 4; i++){
+	for (int i = 0; i < 4; i++){
+		GPIO_WriteToOutputPin(&LED[i], ENABLE);
+		delay(3);
+		GPIO_WriteToOutputPin(&LED[i], DISABLE);
 
-			GPIO_WriteToOutputPin(&LED[i], ENABLE);
-			delay();
-			GPIO_WriteToOutputPin(&LED[i], DISABLE);
-
-		}
 	}
-
 }
