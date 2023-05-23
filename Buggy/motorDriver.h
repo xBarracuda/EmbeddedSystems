43f358a040a0ac26000430f917daa4fc92357ea6
Aruetiise <julius@becker-julius.de>
2023-05-23 15:49:21 +0200

    defines for PCA9685

---
+++ Buggy/motorDriver.h
@@ -0,0 +1,41 @@
+#define LED8_Base 0b00100110
+#define LED8_ON_L  LED8_Base 
+#define LED8_ON_H  LED8_Base + 1
+#define LED8_OFF_L LED8_Base + 2
+#define LED8_OFF_H LED8_Base + 3
+
+#define LED9_Base 0b00101010 
+#define LED9_ON_L  LED9_Base
+#define LED9_ON_H  LED9_Base +1
+#define LED9_OFF_L LED9_Base +2
+#define LED9_OFF_H LED9_Base +3
+
+#define LED10_Base 0b00101110 
+#define LED10_ON_L  LED10_Base
+#define LED10_ON_H  LED10_Base +1
+#define LED10_OFF_L LED10_Base +2
+#define LED10_OFF_H LED10_Base +3
+
+#define LED11_Base 0b00110010 
+#define LED11_ON_L  LED11_Base
+#define LED11_ON_H  LED11_Base +1
+#define LED11_OFF_L LED11_Base +2
+#define LED11_OFF_H LED11_Base +3
+
+#define LED12_Base 0b00110110
+#define LED12_ON_L  LED12_Base
+#define LED12_ON_H  LED12_Base +1
+#define LED12_OFF_L LED12_Base +2
+#define LED12_OFF_H LED12_Base +3
+
+#define LED13_Base 0b00111010
+#define LED13_ON_L  LED13_Base
+#define LED13_ON_H  LED13_Base +1
+#define LED13_OFF_L LED13_Base +2
+#define LED13_OFF_H LED13_Base +3
+
+#define LED_ENABLE_L 0xff
+#define LED_ENABLE_H 0x0f
+
+#define LED_DISABLE_L 0x00
+#define LED_DISABLE_H 0x00
