8b36eeddfbe9c1e77d7fda118b56a485e1ba1c3e
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-04 16:22:01 +0200

    gyro fixes

--- Buggy/gyroDriver.cpp
+++ Buggy/gyroDriver.cpp
@@ -5,6 +5,8 @@
 int Gyro::readGyroAxis(int axis){
     bcm2835_i2c_begin();
     bcm2835_i2c_setSlaveAddress(0x68);
+    bcm2835_i2c_setClockDivider(10000);
+
     char buffer[1];
     buffer[0] = (char)(gyroAxisAddr + axis*2);
     bcm2835_i2c_write(buffer, 1);
