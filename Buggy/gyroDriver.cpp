2dcf81f5ee50bdf5c2bb6b9d153faf483ae94bfc
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-04 16:05:18 +0200

    gyro fixes

--- Buggy/gyroDriver.cpp
+++ Buggy/gyroDriver.cpp
@@ -3,22 +3,24 @@
 #include <bcm2835.h>
 
 int Gyro::readGyroAxis(int axis){
+    std::cout << "reingesprungen\n";
     bcm2835_i2c_begin();
     bcm2835_i2c_setSlaveAddress(0x68);
-
+    std::cout << "slave adress set\n";
     char buffer[1];
     buffer[0] = (char)(gyroAxisAddr + axis*2);
     bcm2835_i2c_write(buffer, 1);
     bcm2835_i2c_read(buffer, 1);
 
     int value = (int)(buffer[0] << 8);
-
+    std::cout << "high-bit set\n";
     buffer[0] = (char)(gyroAxisAddr + axis*2 + 1);
     bcm2835_i2c_write(buffer, 1);
     bcm2835_i2c_read(buffer, 1);
     bcm2835_i2c_end();
 
     value |=  (int)(buffer[0]);
+    std::cout << "low-bit set\n";
     std::cout << value;
     return value;
 }
