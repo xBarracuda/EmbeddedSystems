8527e5e3107b9ab42a3fad4ac8010db45ea3495a
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-06 12:33:03 +0200

    Buggy.cpp Buggy.h auskommentiert

--- Buggy/Buggy.h
+++ Buggy/Buggy.h
@@ -1,15 +1,15 @@
-#include "motorDriver.h"
-#include "gyroDriver.h"
-
-class Buggy{
-    private:
-    Motor motorRechts = Motor(0);
-    Motor motorLinks = Motor(1);
-
-    Gyro gyroskop = Gyro();
-
-    public:
-    Buggy();
-    void drive(float speed, int command);
-    void turn(float angle, float speed);
-};
+//#include "motorDriver.h"
+//#include "gyroDriver.h"
+//
+//class Buggy{
+//    private:
+//    Motor motorRechts = Motor(0);
+//    Motor motorLinks = Motor(1);
+//
+//    Gyro gyroskop = Gyro();
+//
+//    public:
+//    Buggy();
+//    void drive(float speed, int command);
+//    void turn(float angle, float speed);
+//};
