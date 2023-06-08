375de84d35d150225e7ce648db7577950ec356bb
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-08 13:03:13 +0200

    buggy test

--- Buggy/main.cpp
+++ Buggy/main.cpp
@@ -18,18 +18,19 @@ void signalHandler(int signum)
 {
     cout << "\n Strg-C Programmende" << endl;
 	// Beenden Sie hier bitte alle Verbindung zu den Sensoren etc.
+    releaseMotors();
     bcm2835_close();
     exit(signum);
 }
 
 int main()
 {   
-    std::cout << "Start" << std::endl;
-    Buggy buggy(1, 4);
     signal(SIGINT, signalHandler);
-
     if (!bcm2835_init())
         return 1;
+
+    Buggy buggy(1, 4);
+
     /*
     Gyro gyro;
     //gyro.initializeGyro();
@@ -48,14 +49,14 @@ int main()
         bcm2835_delay(20);
     }
     */
-    /*
+    
     buggy.drive(100);
     std::cout << "Geerade aus" << std::endl;
     bcm2835_delay(1000);
     std::cout << "Starte Kurve" << std::endl;
     buggy.curve(100,90,100);
     std::cout << "Kurve fertig" << std::endl;
-    */
+    
     // Csignal für Abbruch über STRG-C
     for (;;);
     return 0;
