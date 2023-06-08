3fcf9d378f8ceba60bddb26574f42b0553e13107
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-08 09:49:04 +0200

    ultraschall test delay

--- Buggy/UltraSchallDriver.cpp
+++ Buggy/UltraSchallDriver.cpp
@@ -17,7 +17,7 @@ int UltraSchall::getDistance()
 	bcm2835_delayMicroseconds(30);
 	bcm2835_gpio_write(Trigger, LOW);
 
-	bcm2835_delayMicroseconds(475);
+	bcm2835_delayMicroseconds(1000);
 
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now(); 
 
