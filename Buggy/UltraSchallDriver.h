8cc029e3ecf84748bfd2d37f1b941bda3b763a0a
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-08 10:21:39 +0200

    ultraschall speec dof souns

--- Buggy/UltraSchallDriver.h
+++ Buggy/UltraSchallDriver.h
@@ -1,5 +1,5 @@
 #pragma once
-#define CMproMikrosekunde 0.000003432f //Schallgeschwindigkeit (343.2m/s) in CM pro Sekunde dividiert durch 10^6 um auf CM pro Mikrosekunde zu kommen
+#define CMproMikrosekunde 0.03432f //Schallgeschwindigkeit (343.2m/s) in CM pro Sekunde dividiert durch 10^6 um auf CM pro Mikrosekunde zu kommen
 
 class UltraSchall {
 private:
