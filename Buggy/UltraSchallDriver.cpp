5322f23abd7154e4604a5f8d413db077a0a7c82f
mert_ <mert.karakoese@alumni.fh-aachen.de>
2023-06-08 10:04:02 +0200

    no compiler optimizat ion

--- Buggy/UltraSchallDriver.cpp
+++ Buggy/UltraSchallDriver.cpp
@@ -21,11 +21,8 @@ int UltraSchall::getDistance()
 	bcm2835_delayMicroseconds(480);
 
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now(); 
-	int i = 0;
 	while (bcm2835_gpio_lev(Echo) > 0) {
-		i++;
 	}
-	std::cout << "i: " << i << std::endl;
 
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
